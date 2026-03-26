a179cbce12ad295e6a88ba8ee6707fa618f1b7f6
websuraj-prog <surajvishawakarma01@gmail.com>
2026-03-26 16:38:08 +0530

    settings.py updated

--- shopzone/manage.py
+++ shopzone/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopzone.ecomm.settings')\n    try:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomm.settings')
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
